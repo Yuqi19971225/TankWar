9142bd805c4d9d6fb79236aea28c07ebfe5e0df3
Fang <857559565@qq.com>
2021-12-14 20:36:16 +0800

    用双缓冲解决坦克闪烁问题

--- src/TankFrame.java
+++ src/TankFrame.java
@@ -7,10 +7,11 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
     Tank myTank = new Tank(200, 200, Dir.DOWN);
     Bullet b= new Bullet(300,300,Dir.DOWN);
+    static final int GAME_WIDTH=800,GAME_HEIGHT=600;
 
     public TankFrame() {
         setVisible(true);
-        setSize(800, 600);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("Tank War");
 
@@ -22,6 +23,22 @@ public class TankFrame extends Frame {
                 System.exit(0);
             }
         });
+    }
+
+    //双缓冲解决屏幕闪烁问题
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g){
+        if(offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage,0,0,null);
     }
 
     @Override
